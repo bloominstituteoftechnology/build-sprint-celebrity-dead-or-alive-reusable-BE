5e9d48807ff9d730d32b1614ae3a2e907f1b9846
drewgoenner <52584951+drewgoenner@users.noreply.github.com>
2019-10-21 14:03:56 -0500

    trying to get sample users to work

--- data/seeds/001-sample-users.js
+++ data/seeds/001-sample-users.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'flash', password: 'wallysux', score: 120},
-        {id: 2, username: 'wonderwoman', password: 'balady', score: 92},
-        {id: 3, username: 'swampthing', password: 'greenisgood', score: 950}
+        { 'username': "flash", 'password': "wallysux", 'score': 120},
+        { 'username': 'wonderwoman', 'password': 'balady', 'score': 92},
+        { 'username': 'swampthing', 'password': 'greenisgood', 'score': 950}
       ]);
     });
 };
